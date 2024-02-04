7b4525df81d610580556ea13574bd7d42bde67a0
twoics <second_vozhzhov@mail.ru>
2024-02-04 14:06:28 +0700

    feat: Добавил di для получения админа

--- app/dependencies/auth.py
+++ app/dependencies/auth.py
@@ -1,10 +1,9 @@
-from typing import Annotated
-
 from fastapi import Depends
 from fastapi import HTTPException
 from fastapi.security import OAuth2PasswordBearer
 from starlette import status
 
+from app.config.config import Settings
 from app.dependencies.config import get_settings
 from app.models import User
 from app.repo.user import UserRepository
@@ -13,15 +12,15 @@ from app.services.auth.tokens import TokenAuth
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl='auth/login')
 
 
-def get_auth_service(conf: Annotated[get_settings, Depends()]) -> TokenAuth:
+def get_auth_service(conf: Settings = Depends(get_settings)) -> TokenAuth:
     """Get auth service customized by application configuration"""
 
     return TokenAuth(conf)
 
 
 async def get_current_user(
-    access_token: Annotated[str, Depends(oauth2_scheme)],
-    auth_service: Annotated[TokenAuth, Depends(get_auth_service)],
+    access_token: OAuth2PasswordBearer = Depends(oauth2_scheme),
+    auth_service: TokenAuth = Depends(get_auth_service),
 ) -> User:
     """Validate access token and return user by this token"""
 
@@ -30,7 +29,7 @@ async def get_current_user(
     return await UserRepository.get_by_id(claims['sub'])
 
 
-async def get_admin_user(user: Annotated[get_current_user, Depends()]) -> User:
+async def get_admin_user(user: User = Depends(get_current_user)) -> User:
     """Validate access token and return user by this token if user is admin"""
 
     if not user.is_admin:
