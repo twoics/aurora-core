195ff52ecdbaf43b39fb1caa1af30e295bbd4a79
twoics <second_vozhzhov@mail.ru>
2024-02-04 14:33:11 +0700

    feat: Заменил UserRepository на di

--- app/dependencies/auth.py
+++ app/dependencies/auth.py
@@ -5,6 +5,7 @@ from starlette import status
 
 from app.config.config import Settings
 from app.dependencies.config import get_settings
+from app.dependencies.repo import user_repo
 from app.models import User
 from app.repo.user import UserRepository
 from app.services.auth.tokens import TokenAuth
@@ -21,12 +22,13 @@ def get_auth_service(conf: Settings = Depends(get_settings)) -> TokenAuth:
 async def get_current_user(
     access_token: OAuth2PasswordBearer = Depends(oauth2_scheme),
     auth_service: TokenAuth = Depends(get_auth_service),
+    repo: UserRepository = Depends(user_repo),
 ) -> User:
     """Validate access token and return user by this token"""
 
     if not (claims := await auth_service.decode_token(access_token)):
         raise HTTPException(status.HTTP_403_FORBIDDEN, detail='Access denied')
-    return await UserRepository.get_by_id(claims['sub'])
+    return await repo.get_by_id(claims['sub'])
 
 
 async def get_admin_user(user: User = Depends(get_current_user)) -> User:
--- app/routes/auth.py
+++ app/routes/auth.py
@@ -2,11 +2,12 @@ from fastapi import APIRouter
 from fastapi import Body
 from fastapi import Depends
 from fastapi import HTTPException
+from fastapi.security import OAuth2PasswordRequestForm as OAuthForm
 from starlette import status
 from starlette.responses import Response
 
-from app.dependencies import get_auth_service
-from app.dto.user import UserLogin
+from app.dependencies.auth import get_auth_service
+from app.dependencies.repo import user_repo
 from app.dto.user import UserRegister
 from app.repo.user import UserRepository
 from app.services.auth.credentials import verify_password
@@ -16,26 +17,35 @@ router = APIRouter()
 
 
 @router.post('/register')
-async def register(user: UserRegister = Body(...)):
+async def register(
+    user: UserRegister = Body(...),
+    repo: UserRepository = Depends(user_repo),
+):
     """Register user by username and password"""
 
-    exist_user = await UserRepository.get_by_name(user.username)
+    exist_user = await repo.get_by_name(user.username)
     if exist_user:
         raise HTTPException(status.HTTP_400_BAD_REQUEST, detail='User already exists')
 
-    await UserRepository.create_user(UserRegister(**user.dict()))
+    await repo.create_user(UserRegister(**user.dict()))
     return Response(status.HTTP_201_CREATED)
 
 
 @router.post('/login')
 async def login(
-    login_user: UserLogin = Body(...),
+    form_data: OAuthForm = Depends(OAuthForm),
     auth_service: TokenAuth = Depends(get_auth_service),
+    repo: UserRepository = Depends(user_repo),
 ):
     """Login user by username and password"""
 
-    exist_user = await UserRepository.get_by_name(login_user.username)
-    if not exist_user or not verify_password(exist_user, login_user.password):
+    exist_user = await repo.get_by_name(form_data.username)
+    if not exist_user or not verify_password(exist_user, form_data.password):
         raise HTTPException(status.HTTP_400_BAD_REQUEST, detail='Wrong credentials')
 
     return {**await auth_service.generate_tokens(exist_user)}
+
+
+@router.post('/refresh')
+async def refresh(refresh_token: str, token: TokenAuth = Depends(get_auth_service)):
+    """Refresh token pair by refresh token"""
