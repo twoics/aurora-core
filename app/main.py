660be32f10d5228ac7736795a9b1977b87a1049f
twoics <second_vozhzhov@mail.ru>
2024-02-10 16:34:27 +0700

    feat: Добавил matrix api в main

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 
 from app.config.base import init_database
 from app.routes.auth import router as auth_router
+from app.routes.matrix import router as matrix_router
 from app.routes.user import router as user_router
 
 app = FastAPI()
@@ -14,3 +15,4 @@ async def start_database():
 
 app.include_router(auth_router, tags=['Auth'], prefix='/auth')
 app.include_router(user_router, tags=['User'], prefix='/user')
+app.include_router(matrix_router, tags=['Matrix'], prefix='/matrix')
