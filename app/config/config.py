bd8e4e212a8ead47d177db2a94d3fdc5a798b78a
twoics <second_vozhzhov@mail.ru>
2024-02-11 16:33:00 +0700

    feat: Обновил settings

--- app/config/config.py
+++ app/config/config.py
@@ -8,6 +8,10 @@ class Settings(BaseSettings):
     TIME_ZONE: str
     ACCESS_TOKEN_EXPIRE_MINUTES: int
     REFRESH_TOKEN_EXPIRE_MINUTES: int
+    MQTT_HOST: str
+    MQTT_PORT: int
+    MQTT_USERNAME: str
+    MQTT_PASSWORD: str
 
     class Config:
         env_file = '.env'
