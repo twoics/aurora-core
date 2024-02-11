99713d2393afd3a257d222553eeed2bf74375e83
twoics <second_vozhzhov@mail.ru>
2024-02-11 15:20:34 +0700

    refactor: Вынес _user_in_matrix_group к остальным зависимостям

--- app/dependencies/valid.py
+++ app/dependencies/valid.py
@@ -2,11 +2,21 @@ from fastapi import Depends
 from fastapi import HTTPException
 from starlette import status
 
-from app.dependencies.group import user_in_matrix_group
 from app.dependencies.repo import matrix_repo
 from app.dependencies.repo import user_repo
 from app.repo.matrix.proto import MatrixRepo
 from app.repo.user.proto import UserRepo
+
+
+async def _user_in_matrix_group(
+    uuid: str,
+    username: str,
+    m_repo: MatrixRepo = Depends(matrix_repo),
+    u_repo: UserRepo = Depends(user_repo),
+) -> bool:
+    """Checks if a user in a matrix group"""
+
+    return await m_repo.user_exists(uuid, await u_repo.get_by_name(username))
 
 
 async def matrix_uuid_exist(uuid: str, repo: MatrixRepo = Depends(matrix_repo)):
@@ -23,7 +33,7 @@ async def username_is_exist(username: str, repo: UserRepo = Depends(user_repo)):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
 
 
-async def user_in_group(exists: bool = Depends(user_in_matrix_group)):
+async def user_in_group(exists: bool = Depends(_user_in_matrix_group)):
     """Validate if user in matrix group"""
 
     if not exists:
@@ -32,7 +42,7 @@ async def user_in_group(exists: bool = Depends(user_in_matrix_group)):
         )
 
 
-async def user_not_in_group(exists: bool = Depends(user_in_matrix_group)):
+async def user_not_in_group(exists: bool = Depends(_user_in_matrix_group)):
     """Validate if user not in matrix group"""
 
     if exists:
