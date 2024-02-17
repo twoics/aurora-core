63cabc62404f47cdcbe55a8dedaf7baab9f8e859
twoics <second_vozhzhov@mail.ru>
2024-02-17 21:36:22 +0700

    fix: Исправил логи работы пула соединений с матрицами

--- app/services/pool/clients.py
+++ app/services/pool/clients.py
@@ -1,5 +1,6 @@
 from typing import List
 
+from models import Matrix
 from models import User
 from redis.asyncio import Redis as AsyncRedis
 from services.pool.proto import MatrixConnectionsPoolProto
@@ -9,32 +10,32 @@ class MatrixConnectionsPool(MatrixConnectionsPoolProto):
     def __init__(self, redis: AsyncRedis):
         self._redis = redis
 
-    async def is_connected(self, user: User) -> bool:
+    async def is_connected(self, user: User, matrix: Matrix) -> bool:
         """Check is user still in the pool"""
 
-        key = await self._get_user_key(user)
+        key = await self._get_user_key(user, matrix)
         return bool(await self._redis.exists(key))
 
-    async def connect(self, user):
+    async def connect(self, user: User, matrix: Matrix):
         """Add a new matrix-client connection into pool"""
 
-        key = await self._get_user_key(user)
-        await self._redis.set(name=key, value=user.id)
+        key = await self._get_user_key(user, matrix)
+        await self._redis.set(name=key, value=str(matrix.id))
 
-    async def disconnect(self, user):
+    async def disconnect(self, user: User, matrix: Matrix):
         """Delete user from pool"""
 
-        key = await self._get_user_key(user)
+        key = await self._get_user_key(user, matrix)
         await self._redis.delete(key)
 
-    async def get_all(self) -> List[str]:
-        """Get all users ids from pool"""
+    async def get_user_matrices(self, user: User) -> List[str]:
+        """Get all matrices ids to which the user is connected"""
 
-        keys = await self._redis.keys('m-clients:*')
+        keys = await self._redis.keys(f'm-clients:{str(user.pk)}:*')
         return await self._redis.mget(keys)
 
     @staticmethod
-    async def _get_user_key(user: User) -> str:
+    async def _get_user_key(user: User, matrix: Matrix) -> str:
         """Generate key for user"""
 
-        return f'm-clients:{str(user.pk)}'
+        return f'm-clients:{str(user.pk)}:{str(matrix.id)}'
--- app/services/pool/proto.py
+++ app/services/pool/proto.py
@@ -1,17 +1,18 @@
 from typing import List
 
+from models import Matrix
 from models import User
 
 
 class MatrixConnectionsPoolProto:
-    async def is_connected(self, user: User) -> bool:
+    async def is_connected(self, user: User, matrix: Matrix) -> bool:
         """Check is user still in the pool"""
 
-    async def connect(self, user):
+    async def connect(self, user: User, matrix: Matrix):
         """Add a new matrix-client connection into pool"""
 
-    async def disconnect(self, user):
+    async def disconnect(self, user: User, matrix: Matrix):
         """Delete user from pool"""
 
-    async def get_all(self) -> List[str]:
-        """Get all connected-users ids"""
+    async def get_user_matrices(self, user: User) -> List[str]:
+        """Get all matrices ids to which the user is connected"""
