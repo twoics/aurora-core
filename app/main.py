a6bcd99cae78b4b67b634ba955d7c21dbf13f468
twoics <second_vozhzhov@mail.ru>
2024-02-17 13:03:01 +0700

    feat: Добавил лимит на отправку сообщений по ws

--- app/main.py
+++ app/main.py
@@ -5,17 +5,29 @@ import aiomqtt
 from config.base import init_database
 from dependencies.config import get_settings
 from fastapi import FastAPI
+from fastapi import WebSocketException
+from fastapi_limiter import FastAPILimiter
 from prometheus_fastapi_instrumentator import Instrumentator
+from redis.asyncio import Redis as aredis
 from routes.auth import router as auth_router
 from routes.control import router as rc_router
 from routes.matrix import router as matrix_router
 from routes.user import router as user_router
+from starlette.status import WS_1013_TRY_AGAIN_LATER
+
+
+async def websocket_limit_callback(*_):
+    """Called when a client sends requests over the limit by websocket"""
+
+    raise WebSocketException(WS_1013_TRY_AGAIN_LATER, 'Too Many Requests')
 
 
 @contextlib.asynccontextmanager
 async def lifespan(*_):
     conf = get_settings()
     await init_database()
+    redis_connection = aredis.from_url(conf.REDIS_URL, encoding='utf8')
+    await FastAPILimiter.init(redis_connection, ws_callback=websocket_limit_callback)
     async with aiomqtt.Client(
         conf.MQTT_HOST,
         username=conf.MQTT_USERNAME,
