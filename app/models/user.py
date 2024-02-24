9e35fc4926e57bf59818e5cf5013b04d278165cf
twoics <second_vozhzhov@mail.ru>
2024-02-24 12:40:22 +0700

    feat: Добавил access key пользователю

--- app/models/user.py
+++ app/models/user.py
@@ -5,6 +5,7 @@ from beanie import Indexed
 class User(Document):
     username: Indexed(str, unique=True)
     password: str
+    access_key: str | None = None
     is_admin: bool = False
     is_matrices_access: bool = True
 
