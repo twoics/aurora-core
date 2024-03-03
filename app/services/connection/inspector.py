a64da7a3eaf66884e6bcb42fc1981c5343a4bd1c
twoics <second_vozhzhov@mail.ru>
2024-03-03 14:05:18 +0700

    feat: Добавил класс для созданиия объектов observer

--- app/services/connection/inspector.py
+++ app/services/connection/inspector.py
@@ -1,3 +1,4 @@
+import dataclasses
 import logging
 
 from config.config import Settings
@@ -11,45 +12,63 @@ from starlette.websockets import WebSocket
 logger = logging.getLogger(__name__)
 
 
-class ConnectionObserver:
-    def __init__(
-        self,
-        session: Session,
-        config: Settings,
-        websocket: WebSocket,
-        pool: MatrixConnectionsPool,
-    ):
-        # Set during call aenter
-        self._ratelimit = None
+@dataclasses.dataclass
+class Context:
+    websocket: WebSocket
+    session: Session
+    pool: MatrixConnectionsPool
+    config: Settings
 
-        self._ws = websocket
-        self._pool = pool
-        self._matrix = session.matrix
-        self._user = session.user
-        self._config = config
-        self._client = session.client
+
+class Inspector:
+    def __init__(self, context: Context, ratelimit: WebSocketRateLimiter):
+        self._ratelimit = ratelimit
+        self._ws = context.websocket
+        self._pool = context.pool
+        self._session = context.session
+
+    async def _get_rate_key(self) -> str:
+        """Get a key for the ratelimit"""
+
+        sess_dict = dataclasses.asdict(self._session)
+        user, matrix, client = (
+            sess_dict['user'],
+            sess_dict['matrix'],
+            sess_dict['client'],
+        )
+        return f'{user.id}:{client.id}:{matrix.uuid}'
 
     async def inspect(self) -> None:
         """Check client permissions and raise ws exception if something wrong"""
 
-        if not self._ratelimit:
-            raise ValueError('The method can be called after using with')
-
-        await self._ratelimit(
-            self._ws,
-            context_key=f'{self._user.id}:{self._client.id}:{self._matrix.uuid}',
-        )
-        if not await self._pool.is_connected(self._user, self._matrix):
+        user, matrix = self._session.user, self._session.matrix
+        await self._ratelimit(self._ws, self._get_rate_key())
+        if not await self._pool.is_connected(user, matrix):
             raise WebSocketException(WS_1008_POLICY_VIOLATION, 'Access denied')
 
-    async def __aenter__(self):
+
+class ConnectionObserver:
+    def __init__(self, context: Context):
+        self._ws = context.websocket
+        self._pool = context.pool
+        self._context = context
+        self._matrix, self._user, self._client, self._config = (
+            context.session.matrix,
+            context.session.user,
+            context.session.client,
+            context.config,
+        )
+
+    async def __aenter__(self) -> Inspector:
         """Add user in connection pool"""
 
-        self._ratelimit = WebSocketRateLimiter(
+        limit = WebSocketRateLimiter(
             seconds=1, times=self._config.WS_QUERY_COUNT_PER_SECOND
         )
         await self._pool.connect(self._user, self._matrix)
         logger.info(f'Put {self._user.username}:{self._matrix.uuid} connection to pool')
+
+        return Inspector(ratelimit=limit, context=self._context)
 
     async def __aexit__(self, exc_type, exc_val, exc_tb):
         """Remove user from connection pool"""
@@ -57,3 +76,20 @@ class ConnectionObserver:
         await self._pool.disconnect(self._user, self._matrix)
         logger.info(f'Delete {self._user.username}:{self._matrix.uuid} from pool')
         logger.info('Connection in pool closed')
+
+
+class ObserverFactory:
+    def __init__(
+        self, websocket: WebSocket, pool: MatrixConnectionsPool, config: Settings
+    ):
+        self._ws = websocket
+        self._pool = pool
+        self._config = config
+
+    def get_observer(self, session: Session) -> ConnectionObserver:
+        """Create observer for inspect this session"""
+
+        context = Context(
+            websocket=self._ws, session=session, pool=self._pool, config=self._config
+        )
+        return ConnectionObserver(context=context)
