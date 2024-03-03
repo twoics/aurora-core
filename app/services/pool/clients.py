f524e4f5300c75921a71afc6bf1d9faf316f4b4d
twoics <second_vozhzhov@mail.ru>
2024-03-03 15:23:50 +0700

    feat: Добавил клиента в методы сервиса ConnectionPool

--- app/services/pool/clients.py
+++ app/services/pool/clients.py
@@ -1,6 +1,7 @@
 from typing import List
 
 from config.config import Settings
+from models import Client
 from models import Matrix
 from models import User
 from redis.asyncio import Redis as AsyncRedis
@@ -12,15 +13,15 @@ class RedisConnectionPool(MatrixConnectionsPool):
         self._redis = redis
         self._prefix = f'{conf.GLOBAL_CASH_KEY_PREFIX}:ws:clients'
 
-    async def is_connected(self, user: User, matrix: Matrix) -> bool:
+    async def is_connected(self, client: Client, user: User, matrix: Matrix) -> bool:
         key = await self._get_user_key(user, matrix)
         return bool(await self._redis.exists(key))
 
-    async def connect(self, user: User, matrix: Matrix):
+    async def connect(self, client: Client, user: User, matrix: Matrix):
         key = await self._get_user_key(user, matrix)
         await self._redis.set(name=key, value=matrix.uuid)
 
-    async def disconnect(self, user: User, matrix: Matrix):
+    async def disconnect(self, client: Client, user: User, matrix: Matrix):
         key = await self._get_user_key(user, matrix)
         await self._redis.delete(key)
 
--- app/services/pool/proto.py
+++ app/services/pool/proto.py
@@ -1,17 +1,18 @@
 from typing import List
 
+from models import Client
 from models import Matrix
 from models import User
 
 
 class MatrixConnectionsPool:
-    async def is_connected(self, user: User, matrix: Matrix) -> bool:
+    async def is_connected(self, client: Client, user: User, matrix: Matrix) -> bool:
         """Check is user still in the pool"""
 
-    async def connect(self, user: User, matrix: Matrix):
+    async def connect(self, client: Client, user: User, matrix: Matrix):
         """Add a new matrix-client connection into pool"""
 
-    async def disconnect(self, user: User, matrix: Matrix):
+    async def disconnect(self, client: Client, user: User, matrix: Matrix):
         """Delete user from pool"""
 
     async def get_user_controlled_matrices(self, user: User) -> List[str]:
