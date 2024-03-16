66c4f8e59b0a540e5f49658e9103a2a880cbc335
twoics <second_vozhzhov@mail.ru>
2024-03-16 21:25:49 +0700

    refactor: Настроено лоигрование в middleware

--- app/main.py
+++ app/main.py
@@ -1,11 +1,14 @@
 import contextlib
+import logging
 
+from asgi_correlation_id import CorrelationIdMiddleware
 from config.base import init_database
 from config.redis import get_redis
 from deps.config import get_settings
 from fastapi import FastAPI
 from fastapi import WebSocketException
 from fastapi_limiter import FastAPILimiter
+from middleware.log import LoggingMiddleware
 from prometheus_fastapi_instrumentator import Instrumentator
 from redis.asyncio import Redis as aredis
 from routes.auth import router as auth_router
@@ -14,6 +17,8 @@ from routes.control import router as rc_router
 from routes.matrix import router as matrix_router
 from routes.user import router as user_router
 from starlette.status import WS_1013_TRY_AGAIN_LATER
+
+logger = logging.getLogger(__name__)
 
 
 async def websocket_limit_callback(*_):
@@ -33,15 +38,21 @@ async def clear_cache():
 
 @contextlib.asynccontextmanager
 async def lifespan(*_):
+    logger.info('Start configuring server...')
     conf = get_settings()
     await init_database()
     redis_connection = aredis.from_url(conf.REDIS_URL, encoding='utf8')
     await FastAPILimiter.init(redis_connection, ws_callback=websocket_limit_callback)
+    logger.info('Server started and configured successfully')
     yield
+    logger.info('Server shut down')
 
 
 app = FastAPI(lifespan=lifespan)
 Instrumentator().instrument(app).expose(app)
+
+app.add_middleware(LoggingMiddleware)
+app.add_middleware(CorrelationIdMiddleware)
 
 app.include_router(auth_router, tags=['Auth'], prefix='/auth')
 app.include_router(user_router, tags=['User'], prefix='/user')
