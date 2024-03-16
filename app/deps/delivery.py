c46cf766f5d7a099b6649ce509c1c4dc0201ea23
twoics <second_vozhzhov@mail.ru>
2024-03-16 21:23:45 +0700

    feat: Добавлено отправление изображений в брокер для обработки воркерами

--- app/deps/delivery.py
+++ app/deps/delivery.py
@@ -1,11 +1,15 @@
+from config.config import Settings
 from config.redis import get_redis
+from deps.config import get_settings
 from fastapi import Depends
 from redis.asyncio import Redis as AsyncRedis
 from services.delivery.mqtt import MqttDelivery
 from services.delivery.proto import Delivery
 
 
-def get_delivery(redis: AsyncRedis = Depends(get_redis)) -> Delivery:
+def get_delivery(
+    redis: AsyncRedis = Depends(get_redis), config: Settings = Depends(get_settings)
+) -> Delivery:
     """Get object which can send data to matrix"""
 
-    return MqttDelivery(redis)
+    return MqttDelivery(redis, config)
